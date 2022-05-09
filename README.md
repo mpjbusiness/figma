91896e455ab803329cd15cc8c6c547d2bdfd9c00
mpjbusiness <55766355+mpjbusiness@users.noreply.github.com>
2022-05-09 17:38:57 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fonts
+
+For fonts to be properly imported, you need to copy and paste the `<link/>` elements from `public/index.html` into your `<head>` element
+
+Sample link element for styles: 
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700"
+/>
---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+
+//IMPORTANT NOTE: TO UNDERSTAND HOW THINGS LIKE TEXT IMPORTS WORK, READ THE README
+
+import "./App.css"
+export default function MacBookPro141(props: MacBookPro141Interface) {
+  return (
+    <div className="mac-book-pro141-container">
+      <div className="macBookPro142">
+        <div className="rectangle3"></div>
+        <div className="rectangle4"></div>
+        <p className="gETStarted1">GET STARTED</p>
+        <p className="getStartedWithFat1">Get started with Fathym for free.</p>
+        <p className="fromFigmaFileToA1">
+          From Figma file to a hosted landing page in minutes.
+        </p>
+        <div className="thisSimpleLanding2">
+          <p className="thisSimpleLanding3">
+            This simple landing page was quickly mocked up in Figma. Then we
+            used the FireJet plugin to&nbsp;
+            <br />
+            transform the design into readable React code.&nbsp;
+            <br />
+            <br />
+            From here we could host that React code as an application on Fathym.
+            <br />
+            <br />
+            Fathym is a platform for hosting, managing and deploying websites
+            and web applications with
+            <br />
+            the utmost flexibility and speed.
+            <br />
+            <br />
+          </p>
+          <p className="learnMoreOnHowWe1">
+            Learn more on how we did it in this blog.
+          </p>
+        </div>
+        <div className="fathymLogoAqua112"></div>
+      </div>
+    </div>
+  )
+}
+
+MacBookPro141.defaultProps = {
+  variant: 'DEFAULT',
+}
+
+interface MacBookPro141Interface {
+  variant: 'DEFAULT';
+}
+
