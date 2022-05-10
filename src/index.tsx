6b5c08a57cf11dd4aebd29451e362cd07bbe5204
todd.german <todd.german@fathym.com>
2022-05-10 12:31:45 -0600

    lots of build fixes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-const rootElement = document.getElementById("app");
+const rootElement = document.getElementById("app") as HTMLElement;
 const root = createRoot(rootElement);
 
 root.render(
